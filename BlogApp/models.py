f8f1944aace1345643a236a506ce2f9e78157cc8
Jeffrey Bekker <jef.bekker@gmail.com>
2024-12-07 02:41:57 +0100

    urls and post page updated, is now dymanic

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -7,3 +7,5 @@ class Post(models.Model):
     body = models.CharField(max_length=100000)
     created_at = models.DateTimeField(default=datetime.now, blank=True)
     
+    def __str__(self):
+        return self.title
--- BlogApp/views.py
+++ BlogApp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all() # To get all the objects from the database (Post model)
+    return render(request, 'index.html', {'posts': posts})
+
+def post(request, pk):
+    single_post = Post.objects.get(id=pk)
+    return render(request, 'post.html', {'single_post': single_post})
